f743ae09a3e77a90183e2ea065f70fe448a60a7b
Fredrik Olovsson <fogithub@gmail.com>
2025-05-08 00:30:28 +0800

    call trpc procedures from client with react-query

--- examples/minimal-react/client/src/Greeting.tsx
+++ examples/minimal-react/client/src/Greeting.tsx
@@ -1,8 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
+import * as React from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { trpc } from './utils/trpc';
 
 export function Greeting() {
-  const greeting = useQuery(trpc.greeting.queryOptions({ name: 'tRPC user' }));
+  const todoCreator = useMutation(trpc.apiCreateTodo.mutationOptions());
 
-  return <div>{greeting.data?.text}</div>;
+  React.useEffect(() => {
+    todoCreator.mutate({ p_task: "New task" + Date.now(), p_due: undefined });
+  }, [])
+
+  const todos = useQuery(trpc.apiReadTodos.queryOptions({
+    p_limit: 10000,
+    p_offset: 0,
+    p_sort_column: "due",
+    p_sort_order: "desc",
+  }));
+
+  return <div>todos: {JSON.stringify(todos.data)}</div>;
 }
