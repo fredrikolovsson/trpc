e1f5e7dbf2ad01240242b3ceae8690950f6d51cc
Fredrik Olovsson <fogithub@gmail.com>
2025-05-08 00:30:08 +0800

    define trpc routes calling postgrest rpc

--- examples/minimal-react/server/index.ts
+++ examples/minimal-react/server/index.ts
@@ -6,6 +6,7 @@ import { initTRPC } from '@trpc/server';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 import cors from 'cors';
 import { z } from 'zod';
+import * as schemas from './generated-schemas';
 
 const t = initTRPC.create();
 
@@ -13,6 +14,45 @@ const publicProcedure = t.procedure;
 const router = t.router;
 
 const appRouter = router({
+  apiCreateTodo: publicProcedure
+    .input(schemas.apiCreateTodoArgsSchemaSchema)
+    .mutation(async ({ input }) => {
+      const response = await fetch('http://localhost:4000/rpc/create_todo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ p_task: input.p_task, p_due: input.p_due }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+
+      const result = await response.json() as z.infer<typeof schemas.apiCreateTodoReturnsSchemaSchema>
+
+      return result
+    }),
+  apiReadTodos: publicProcedure.input(schemas.apiReadTodosArgsSchemaSchema).query(async ({ input }) => {
+    const response = await fetch('http://localhost:4000/rpc/read_todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        p_limit: input.p_limit,
+        p_offset: input.p_offset,
+        p_sort_column: input.p_sort_column,
+        p_sort_order: input.p_sort_order,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+
+    return response.json();
+  }),
   greeting: publicProcedure
     // This is the input schema of your procedure
     // 💡 Tip: Try changing this and see type errors on the client straight away
